aeda85976adb6888de0fa187500c3a22779ce2a8
FelipeSulser <felipesulser@gmail.com>
2017-03-14 12:08:21 +0100

    Added the new version

---
+++ Exercise 1/script.py
@@ -0,0 +1,59 @@
+import csv
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+regex1 = r"\d+"
+regex2 = r"c\d+"
+A = np.zeros((1000,10000)) #1.000x10.000 matrix
+#load data
+with open("data_train.csv") as csvfile:
+	readCSV = csv.reader(csvfile,delimiter=',')
+	next(readCSV) #discard header
+	for row in readCSV:
+		match = re.search(regex1,row[0])
+		match2 = re.search(regex2,row[0])
+		row_i = int(match.group(0))-1
+		col_i = int(match2.group(0)[1:]) -1
+		A[col_i,row_i] = int(row[1])
+
+means = np.mean(A,axis=0)
+for x in range(A.shape[0]): #x is cols
+	for y in range(A.shape[1]):
+		if A[x,y]==0:
+			A[x,y] = means[x]
+
+for m in range(2):
+
+	#SVD
+	U, D, V = np.linalg.svd(A, full_matrices=False)
+
+	D = np.append(D[0:20],np.zeros((980)))
+	print(D)
+	D = np.diag(D)
+
+	print(D.shape)
+
+	Uprime = np.dot(U,np.sqrt(D))
+	Vprime = np.dot(np.sqrt(D),V)
+
+	print(U.shape, D.shape, V.shape)
+	print(Uprime.shape, Vprime.shape)
+	B = np.dot(U,np.dot(D,V))
+	print(np.isclose(A,B).all())
+
+	#now lets predict the data from samplesubmission
+	fout = open('mysubmission.csv', 'w')
+	fout.write("Id,Prediction\n")
+	with open("SampleSubmission.csv") as csvfile:
+	    readCSV = csv.reader(csvfile,delimiter=',')
+	    next(readCSV) #discard header
+	    for row in readCSV:
+	        match = re.search(regex1,row[0])
+	        match2 = re.search(regex2,row[0])
+	        row_i = int(match.group(0))-1
+	        col_i = int(match2.group(0)[1:]) -1
+	        calc = np.dot(Uprime[col_i,:],Vprime.T[row_i,:])
+	        A[col_i,row_i] = calc
+	        fout.write(row[0]+","+str(calc)+"\n")
+	fout.close()
