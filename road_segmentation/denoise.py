61acb50e0db6cea5563450036dbb567fb616a7cd
FelipeSulser <felipesulser@gmail.com>
2017-05-16 18:29:14 +0200

    New postprocesssing

--- road_segmentation/denoise.py
+++ road_segmentation/denoise.py
@@ -9,16 +9,41 @@ import scipy.misc
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+from sklearn.utils.testing import SkipTest
 from sklearn.utils.fixes import sp_version
-from skimage import data, img_as_float
+from skimage import img_as_float
+from scipy import signal
+from scipy.signal import convolve2d
+from skimage import color, data, restoration
 
 NEIGHBOOR_TO_CONSIDER = 8
 BALCK_TH = int(0.75 * NEIGHBOOR_TO_CONSIDER)
 WHITE_TH = int(0.25 * NEIGHBOOR_TO_CONSIDER)
 
-
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def binarize(img,block_size):
+    threshold = 0.3 #more threshold, less road
+    imgwidth = img.shape[0]
+    imgheight = img.shape[1]
+    
+    numblockwidth = int(imgwidth/block_size)
+
+    numblockheight = int(imgheight/block_size)
+
+    for i in range(0,numblockwidth-1):
+        for j in range(0, numblockheight-1):
+            pixel_i = i*block_size
+            pixel_j = j*block_size
+            avg = np.mean(img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size])
+            if(avg > threshold):
+                img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size] = 1
+            else:
+                img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size] = 0
+
+    return img
+
 def remove_filtering_neighbors(img,black_threshold, block_size = 16):
     #img is b&w array with 0 or 1
     imgwidth = img.shape[0]
@@ -28,8 +53,8 @@ def remove_filtering_neighbors(img,black_threshold, block_size = 16):
 
     numblockheight = int(imgheight/block_size)
 
-    for i in range(1,numblockwidth-1):
-        for j in range(1, numblockheight-1):
+    for i in range(0,numblockwidth-1):
+        for j in range(0, numblockheight-1):
             pixel_i = i*block_size
             pixel_j = j*block_size
 
@@ -78,20 +103,32 @@ def remove_filtering_neighbors(img,black_threshold, block_size = 16):
                         for yy in range(0,block_size):
                             img[pixel_i+xx,pixel_j+yy] = 0.0
 
+
     return img
-save_dir = "predictions_test/result_denoised/"
+save_dir = "/Users/felipesulser/Dropbox/ETH/CIL/TensorFlow/predictions_test/result_denoised/"
 if not os.path.isdir(save_dir):
     os.mkdir(save_dir) 
 for i in range(1, 51):
-    mydir = "predictions_test/result/"
+    mydir = "/Users/felipesulser/Dropbox/ETH/CIL/TensorFlow/predictions_test/result/"
     imageid = "prediction_"+str(i)
     image_filename = mydir +imageid+ ".png"
     img = mpimg.imread(image_filename)
     img = rgb2gray(img)
 
-    img_denoised = remove_filtering_neighbors(img, BALCK_TH)
+    
+    #k = np.ones((10,10))
+    #sig2 = signal.convolve2d(img,k,boundary='symm',mode='same')
+    
+    #val = remove_filtering_neighbors(sig2,8)
+    psf = np.ones((8,8))/64
+    deconvolved = restoration.wiener(img,psf,1100)
+    print(deconvolved)
+    deconvolved = deconvolved/np.max(deconvolved)
+    deconvolved = binarize(deconvolved,16)
+    #img_denoised = remove_filtering_neighbors(deconvolved,7,block_size=16)
+    filtered = remove_filtering_neighbors(deconvolved,7,block_size=16)
     save_str = save_dir+imageid+".png"
-    scipy.misc.imsave(save_str,img_denoised)
+    scipy.misc.imsave(save_str,deconvolved)
 
 
 
