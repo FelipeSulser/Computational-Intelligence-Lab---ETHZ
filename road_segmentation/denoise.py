abc4e396adca2f852b594a9696b7d5b8cad3f397
FelipeSulser <felipesulser@gmail.com>
2017-05-18 13:44:01 +0200

    Total Variation denoising. Good results

--- road_segmentation/denoise.py
+++ road_segmentation/denoise.py
@@ -15,6 +15,10 @@ from skimage import img_as_float
 from scipy import signal
 from scipy.signal import convolve2d
 from skimage import color, data, restoration
+from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
+                                 denoise_wavelet, estimate_sigma)
+from skimage import data, img_as_float, color
+from skimage.util import random_noise
 
 NEIGHBOOR_TO_CONSIDER = 8
 BALCK_TH = int(0.75 * NEIGHBOOR_TO_CONSIDER)
@@ -23,8 +27,8 @@ WHITE_TH = int(0.25 * NEIGHBOOR_TO_CONSIDER)
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-def binarize(img,block_size):
-    threshold = 0.3 #more threshold, less road
+def binarize(img,block_size,threshold):
+    
     imgwidth = img.shape[0]
     imgheight = img.shape[1]
     
@@ -97,7 +101,7 @@ def remove_filtering_neighbors(img,black_threshold, block_size = 16):
 
                 sum_val = np.sum(neighbors)
                 wh_threshold = NEIGHBOOR_TO_CONSIDER-black_threshold
-                if(sum_val <= wh_threshold):
+                if(sum_val < wh_threshold):
                     #repaint block
                     for xx in range(0,block_size):
                         for yy in range(0,block_size):
@@ -105,10 +109,11 @@ def remove_filtering_neighbors(img,black_threshold, block_size = 16):
 
 
     return img
-save_dir = "/Users/felipesulser/Dropbox/ETH/CIL/TensorFlow/predictions_test/result_denoised/"
+save_dir = "/Users/felipesulser/Dropbox/ETH/CIL/TensorFlow/predictions_test/denoised/"
 if not os.path.isdir(save_dir):
     os.mkdir(save_dir) 
 for i in range(1, 51):
+    print("Denoising img: "+str(i))
     mydir = "/Users/felipesulser/Dropbox/ETH/CIL/TensorFlow/predictions_test/result/"
     imageid = "prediction_"+str(i)
     image_filename = mydir +imageid+ ".png"
@@ -118,17 +123,35 @@ for i in range(1, 51):
     
     #k = np.ones((10,10))
     #sig2 = signal.convolve2d(img,k,boundary='symm',mode='same')
-    
+    img_bw = binarize(img,16,0.33)
+
     #val = remove_filtering_neighbors(sig2,8)
-    psf = np.ones((8,8))/64
+    psf = np.ones((16,16))/64 #filter of 16x16
     deconvolved = restoration.wiener(img,psf,1100)
-    print(deconvolved)
+    #print(deconvolved)
     deconvolved = deconvolved/np.max(deconvolved)
-    deconvolved = binarize(deconvolved,16)
+    deconvolved = binarize(deconvolved,16,0.33)
+   
     #img_denoised = remove_filtering_neighbors(deconvolved,7,block_size=16)
-    filtered = remove_filtering_neighbors(deconvolved,7,block_size=16)
+    filtered = remove_filtering_neighbors(img,7,block_size=16)
+    filtered = binarize(filtered,16,0.33)
+
+
+    tv_denoise = denoise_tv_chambolle(img, weight=10)
+    tv_denoise = binarize(tv_denoise,16,0.5)
+    tv_denoise = remove_filtering_neighbors(tv_denoise,7,block_size=16)
+
+
+    #IMAGE PLOT TO COMPARE DENOISING RESULTS BETWEEN DIFFERENT METHODS
+
+    #f, axarr = plt.subplots(2,2)
+    #axarr[0,0].imshow(img_bw)
+    #axarr[0,1].imshow(deconvolved)
+    #axarr[1,0].imshow(filtered)
+    #axarr[1,1].imshow(tv_denoise)
+    #plt.show()
     save_str = save_dir+imageid+".png"
-    scipy.misc.imsave(save_str,deconvolved)
+    scipy.misc.imsave(save_str,tv_denoise)
 
 
 
