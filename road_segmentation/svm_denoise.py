9ad8ca0d98986bf4590eb0c5e875fb76d59cb433
Felipe Sulser <felipesulser@gmail.com>
2017-05-23 15:56:09 +0200

    New svm

--- road_segmentation/svm_denoise.py
+++ road_segmentation/svm_denoise.py
@@ -6,6 +6,7 @@ import matplotlib.image as mpimg
 from tensorflow.python.framework import ops
 import scipy.misc
 from sklearn import svm
+import math
 from sklearn import linear_model
 import pickle
 from sklearn.externals import joblib
@@ -13,102 +14,236 @@ import os
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import log_loss
-#ops.reset_default_graph()
-
-TRAIN = True
+from sklearn.ensemble import RandomForestClassifier
+TRAIN = False #If false, then predict
 PATCH_SIZE = 16
-CONTEXT_SIZE = 3 # means that for patch i,j we consider the square i-ps*3,j-ps*3 to i+ps*3, j+ps*3
+CONTEXT_SIZE = 5 # means that for patch i,j we consider the square i-ps*3,j-ps*3 to i+ps*3, j+ps*3
 # Create graph
 total_pixel_length = PATCH_SIZE+2*CONTEXT_SIZE*PATCH_SIZE
 #sess = tf.Session()
+IMG_SIZE = 608
+NEIGHBOOR_TO_CONSIDER = 8
 
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def binarize(img,block_size,threshold):
+    
+    imgwidth = img.shape[0]
+    imgheight = img.shape[1]
+    
+    numblockwidth = int(imgwidth/block_size)
+
+    numblockheight = int(imgheight/block_size)
+
+    for i in range(0,numblockwidth):
+        for j in range(0, numblockheight):
+            pixel_i = i*block_size
+            pixel_j = j*block_size
+            avg = np.mean(img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size])
+            if(avg > threshold):
+                img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size] = 1
+            else:
+                img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size] = 0
+
+    return img
+
+def remove_filtering_neighbors(img,black_threshold, block_size = 16):
+    #img is b&w array with 0 or 1
+    imgwidth = img.shape[0]
+    imgheight = img.shape[1]
+    
+    numblockwidth = int(imgwidth/block_size)
+
+    numblockheight = int(imgheight/block_size)
+
+    for i in range(1,numblockwidth-2):
+        for j in range(1, numblockheight-2):
+            pixel_i = i*block_size
+            pixel_j = j*block_size
+
+            if img[pixel_i,pixel_j] == 0: #if patch is black
+                #if not surrounded by 3 cut it
+                neighbors = np.zeros(8)
+
+                #black is 0
+
+                neighbors[0] = img[pixel_i-block_size,pixel_j]
+                neighbors[1] = img[pixel_i+block_size,pixel_j]
+                neighbors[2] = img[pixel_i,pixel_j-block_size]
+                neighbors[3] = img[pixel_i,pixel_j+block_size]
+                neighbors[4] = img[pixel_i-block_size,pixel_j-block_size]
+                neighbors[5] = img[pixel_i-block_size,pixel_j+block_size]
+                neighbors[6] = img[pixel_i+block_size,pixel_j-block_size]
+                neighbors[7] = img[pixel_i+block_size,pixel_j+block_size]
+
+                sum_val = np.sum(neighbors)
+                if(sum_val > black_threshold):
+                    #repaint block
+                    print('  Block repainted to BLACK!')
+                    for xx in range(0,block_size):
+                        for yy in range(0,block_size):
+                            img[pixel_i+xx,pixel_j+yy] = 1.0
+            else: #white patch 1
+                #if not surrounded by 3 cut it
+                neighbors = np.zeros(8)
+
+                #black is 0
+
+                neighbors[0] = img[pixel_i-block_size,pixel_j]
+                neighbors[1] = img[pixel_i+block_size,pixel_j]
+                neighbors[2] = img[pixel_i,pixel_j-block_size]
+                neighbors[3] = img[pixel_i,pixel_j+block_size]
+                neighbors[4] = img[pixel_i-block_size,pixel_j-block_size]
+                neighbors[5] = img[pixel_i-block_size,pixel_j+block_size]
+                neighbors[6] = img[pixel_i+block_size,pixel_j-block_size]
+                neighbors[7] = img[pixel_i+block_size,pixel_j+block_size]
+
+
+                sum_val = np.sum(neighbors)
+                wh_threshold = NEIGHBOOR_TO_CONSIDER-black_threshold+1
+                if(sum_val < wh_threshold):
+                    print('  Block repainted to WHITE!')
+                    for xx in range(0,block_size):
+                        for yy in range(0,block_size):
+                            img[pixel_i+xx,pixel_j+yy] = 0.0
+
+
+    return img
+
+def fill_rows_and_cols(img, block_size=16, missing_blocks=3):
+    row_sums = np.sum(img, axis=1)
+    col_sums = np.sum(img, axis=0)
+
+    n_blocks_per_rc = math.ceil(IMG_SIZE / block_size)
+    rc_threshold = IMG_SIZE - (missing_blocks+1) * block_size
+
+
+    for row, row_sum in enumerate(row_sums):
+        if row_sum > rc_threshold:
+            # number of road patches is bigger than the minimum
+            img[row,:] = 1
+
+    for col, col_sum in enumerate(col_sums):
+        if col_sum > rc_threshold:
+            # number of road patches is bigger than the minimum
+            img[:,col] = 1
+    
+    return img
+
+def mean_img_per_patch(img,block_size):
+    
+    imgwidth = img.shape[0]
+    imgheight = img.shape[1]
+    
+    numblockwidth = int(imgwidth/block_size)
+
+    numblockheight = int(imgheight/block_size)
+    newimg = np.zeros((numblockwidth,numblockheight))
+    for i in range(0,numblockwidth):
+        for j in range(0, numblockheight):
+            pixel_i = i*block_size
+            pixel_j = j*block_size
+            avg = np.mean(img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size])
+            newimg[i,j] = avg
+            #img[pixel_i:pixel_i+block_size,pixel_j:pixel_j+block_size] = avg
+
+    return newimg
 
 if TRAIN:
     real_y = []
     train_x = []
     max_val= 0
-    train_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/training/groundtruth/"
-    label_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/training/groundtruth/"
-    for i in range(1, 101):
-        imageid = "satImage_%.3d" % i
+    train_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/training/groundtruth_shuffled/"
+    label_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/training/groundtruth_shuffled/"
+    for xx in range(1, 215):
+        imageid = "satImage_%.3d" % xx
         #imageid = "prediction_"+str(i)
         image_filename = train_img_path +imageid+ ".png"
         img = mpimg.imread(image_filename)
+        newimg = mean_img_per_patch(img,PATCH_SIZE)
+
+        numblockwidth = newimg.shape[0]
+
+        numblockheight = newimg.shape[1]
         #print(img)
         #2D matrix between 0,1
         num_patches = int(img.shape[0]/PATCH_SIZE)
-        for i in range(CONTEXT_SIZE,num_patches-CONTEXT_SIZE):
-            for j in range(CONTEXT_SIZE, num_patches-CONTEXT_SIZE):
-                curr_x = img[(i*PATCH_SIZE - CONTEXT_SIZE*PATCH_SIZE):(i*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE),(j*PATCH_SIZE-CONTEXT_SIZE*PATCH_SIZE):(j*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE)]
-                curr_patch = img[i*PATCH_SIZE:i*PATCH_SIZE+PATCH_SIZE,j*PATCH_SIZE:j*PATCH_SIZE+PATCH_SIZE]
-                #print(np.mean(curr_patch)) 
-                if np.mean(curr_patch) > 0.25:
+        for i in range(CONTEXT_SIZE,numblockwidth-CONTEXT_SIZE):
+            for j in range(CONTEXT_SIZE,numblockheight-CONTEXT_SIZE):
+                curr_x = newimg[i-CONTEXT_SIZE:i+CONTEXT_SIZE+1,j-CONTEXT_SIZE: j+CONTEXT_SIZE+1]
+                curr_x = curr_x.flatten()
+                full_context = CONTEXT_SIZE*2 +1
+                ind_to_remove = int(((full_context-1)/2)* (1+full_context))
+                #curr_x = np.delete(curr_x,ind_to_remove)
+                #img[(i*PATCH_SIZE - CONTEXT_SIZE*PATCH_SIZE):(i*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE),(j*PATCH_SIZE-CONTEXT_SIZE*PATCH_SIZE):(j*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE)]
+                curr_patch = newimg[i,j]
+                mean_val = np.mean(curr_patch)
+                if mean_val > 0.25:
                     real_y.append(1)
                 else:
                     real_y.append(0)
-                #print("CURR_PATCH")
-                #print(curr_patch)
-                #max_val = max(max_val,(i*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE)-(i*PATCH_SIZE - CONTEXT_SIZE*PATCH_SIZE))
+
+
                 flattened = curr_x.flatten()
                 train_x.append(flattened)
                 
 
 
-
+    #BEST PARAMETERS C=10, gamma = 0.1
     train_x = np.asarray(train_x)
     real_y = np.asarray(real_y)
     D = train_x.shape[1]
     print("Finished loading images")
-    print("Computing SVM with RBF kernel")
-    clf = svm.SVC(kernel='rbf',probability=True,class_weight='balanced')
-    #pipe = Pipeline([
-    #    ('est',clf)
-    #    ])
-    #c_params = [0.01,0.1,1]
-    #gammas = [0.1/D,1/D, 10/D]
-    #params = dict(
-    #   est=[clf],
-    #   est__C = c_params,
-    #   est__gamma = gammas
-        
-    #    )
-    gammas = np.array([1,0.1,0.01,0.001,0.0001])
-    grid_dict = dict(gamma=gammas)
-    classifier = GridSearchCV(estimator = clf,param_grid=grid_dict)
-    classifier.fit(train_x,real_y)
-    print("BEST PARAMETERS")
-    print(classifier.best_params_)
-    joblib.dump(classifier, 'svcmodel.pkl') 
+    print("Computing Randomforest model...")
+    #rfc = svm.SVC(C=1,kernel='rbf')
+    rfc = RandomForestClassifier(n_estimators=1000,n_jobs=4)
+    # gammas = np.array([0.1,0.01,0.001])
+    # cs = np.array([10,1,0.1])
+    # grid_dict = dict(gamma=gammas,C=cs)
+    # classifier = GridSearchCV(estimator = clf,param_grid=grid_dict,n_jobs=4,cv=5)
+    # classifier.fit(train_x,real_y)
+    # print("BEST PARAMETERS")
+    # print(classifier.best_params_)
+    rfc.fit(train_x,real_y) 
+    joblib.dump(rfc, 'rfcmodel214.pkl') 
 else:
-    clf = svm.SVC(C=1.0,kernel='rbf')
-    clf = joblib.load('svcmodel.pkl')
+    clf = joblib.load('rfcmodel214.pkl')
     #now predict
     predict_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/predictions_test/result/"
     output_img_path = (os.path.dirname(os.path.realpath(__file__)))+"/predictions_test/result_denoised/"
     print("Predicting patches")
-    for i in range(1,51):
-        imageid = "prediction_"+str(i)
+    for xx in range(1,51):
+        imageid = "prediction_"+str(xx)
         image_filename = predict_img_path+imageid+".png"
+        print("Predicting "+image_filename)
         img = mpimg.imread(image_filename)
+        img = rgb2gray(img)
 
-        num_patches = int(img.shape[0]/PATCH_SIZE)
-        for i in range(CONTEXT_SIZE,num_patches-CONTEXT_SIZE):
-            for j in range(CONTEXT_SIZE, num_patches-CONTEXT_SIZE):
-                curr_x = img[(i*PATCH_SIZE - CONTEXT_SIZE*PATCH_SIZE):(i*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE),(j*PATCH_SIZE-CONTEXT_SIZE*PATCH_SIZE):(j*PATCH_SIZE+CONTEXT_SIZE*PATCH_SIZE)]
-                flattened = curr_x.flatten()
-                flattened = flattened/(np.linalg.norm(flattened)+1)
-                flattened = flattened.reshape(1,-1)
-                meanval = np.mean(img[i*PATCH_SIZE:i*PATCH_SIZE+PATCH_SIZE,j*PATCH_SIZE:j*PATCH_SIZE+PATCH_SIZE])
-                #print("---------")
-                #print("WE HAVE : "+str(meanval))
-                predic = clf.predict(flattened)
-                img[i*PATCH_SIZE:i*PATCH_SIZE+PATCH_SIZE,j*PATCH_SIZE:j*PATCH_SIZE+PATCH_SIZE] = (meanval+predic)/2
-                #print("EXPECTED: "+str(predic))
+        newimg = mean_img_per_patch(img,PATCH_SIZE)
+        numblockwidth = newimg.shape[0]
+        numblockheight = newimg.shape[1]
+        res = []
+        for i in range(CONTEXT_SIZE,numblockwidth-CONTEXT_SIZE):
+           for j in range(CONTEXT_SIZE,numblockheight-CONTEXT_SIZE):
+                curr_x = newimg[i-CONTEXT_SIZE:i+CONTEXT_SIZE+1,j-CONTEXT_SIZE: j+CONTEXT_SIZE+1]
+                curr_x = curr_x.flatten()
+                full_context = CONTEXT_SIZE*2 +1
+                ind_to_remove = int(((full_context-1)/2)* (1+full_context))
+                #curr_x = np.delete(curr_x,ind_to_remove)
+                res.append(curr_x)
+
+        res = np.asarray(res)
+        y_estim = clf.predict(res)
+        it = 0
+        for i in range(CONTEXT_SIZE,numblockwidth-CONTEXT_SIZE):
+           for j in range(CONTEXT_SIZE,numblockheight-CONTEXT_SIZE):
+            img[i*PATCH_SIZE:i*PATCH_SIZE+PATCH_SIZE,j*PATCH_SIZE:j*PATCH_SIZE+PATCH_SIZE] = y_estim[it]
+            it+=1
+        
+        img = binarize(img,PATCH_SIZE,0.4)
+        filtered = remove_filtering_neighbors(img,7,block_size=16)
+        filtered = fill_rows_and_cols(filtered, missing_blocks=3)
         save_str = output_img_path+imageid+".png"
         scipy.misc.imsave(save_str,img)
-
-
-#get output data
-
-
-
